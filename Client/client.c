0c127104490e0570939668aa46f5dd71c0a4d27d
k4rr3 <davidcarreras2003@gmail.com>
2023-02-10 10:35:45 +0100

    .

--- Client/client.c
+++ Client/client.c
@@ -34,7 +34,7 @@ int main(int argc, char *argv[])
 };
 
 
-struct cfg get_cfg(int argc, char *argv[])
+/* struct cfg get_cfg(int argc, char *argv[])
 {
     struct cfg user_cfg;
     char* result;
@@ -61,28 +61,54 @@ struct cfg get_cfg(int argc, char *argv[])
         }
         printf("%s\n", result);
     } 
+ */
+struct cfg get_cfg(int argc, char *argv[])
+{
+    struct cfg user_cfg;
+    char line[MAX_LEN];
+    char *parsed_line;
+    char *file_name = get_params(argc, argv);
+    FILE *file = fopen(file_name, "r");
+    if (!file) {
+        printf("Error opening file: %s\n", file_name);
+        exit(EXIT_FAILURE);
+    }
 
-        
-        
-        
-        
-        
-    
-    
+    for (int i = 0; i < 4; i++) {
+        parsed_line = get_line(line, file);
+        if (!parsed_line) {
+            printf("Error reading line %d\n", i + 1);
+            exit(EXIT_FAILURE);
+        }
 
-    //perror("An error ocurred when opening ''.cfg file");
-    //exit(1);
+        switch (i) {
+        case 0:
+            strncpy(user_cfg.id, parsed_line, MAX_LEN);
+            break;
+        case 1:
+            strncpy(user_cfg.mac, parsed_line, MAX_LEN);
+            break;
+        case 2:
+            strncpy(user_cfg.nms_id, parsed_line, MAX_LEN);
+            break;
+        case 3:
+            strncpy(user_cfg.nms_udp_port, parsed_line, MAX_LEN);
+            break;
+        }
+        printf("%s\n", parsed_line);
+    }
+    fclose(file);
+    return user_cfg;
 };
+
 char* get_line(char line[], FILE* file){
     char* word;
     if ((word = fgets(line,MAX_LEN,file)) != NULL)
         line[strlen(line) - 1] = '\0';
         word = strtok(line, " ");
         word = strtok(NULL, " ");
-        //strcpy((char*) user_cfg.id, result);
-        //printf("%s\n", result);
         return word;
-    perror("An error ocurred when opening ''.cfg file");
+    perror("An error ocurred when opening .cfg file");
     exit(1);
 };
 
