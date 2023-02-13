cf70d764230860ad8c296403f97431e1a4489e1b
k4rr3 <davidcarreras2003@gmail.com>
2023-02-13 11:33:00 +0100

    Update client.c

--- Client/client.c
+++ Client/client.c
@@ -25,18 +25,28 @@ struct pdu_udp
 };
 // DECLARATION
 struct cfg get_cfg(int argc, char *argv[]);
-char *get_params(int argc, char *argv[]);
+char *get_file_name(int argc, char *argv[]);
 char *get_line(char line[], FILE *file);
 void show_status(int status);
 
 int main(int argc, char *argv[])
 {
     int status = 0; // 0 == DISCONNECTED
+    int debug = check_debug_mode(argc, argv);
     struct cfg user_cfg = get_cfg(argc, argv);
     show_status(status);
     connection_phase(status, user_cfg);
 }
-
+int check_debug_mode(int argc, char *argv[]){
+     for (int i = 0; i < argc; i++)
+    {
+        if ((strcmp("-d", argv[i]) == 0))
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
 void show_status(int status)
 {
     time_t current_time;
@@ -65,40 +75,13 @@ void show_status(int status)
         break;
     }
 }
-/* struct cfg get_cfg(int argc, char *argv[])
-{
-    struct cfg user_cfg;
-    char* result;
-    char* file_name = get_params(argc, argv);
-    char line[MAX_LEN];
-    FILE* file = fopen(file_name, "r");
-     for (int i = 0; i < 4; i++)
-    {
-        //printf("%i", i);
-        result = get_line(line, file);
-        switch (i)
-        {
-        case 0:
-            strcpy(user_cfg.id, result);
-            break;
-        case 1:
-            strcpy(user_cfg.mac, result);
-        case 2:
-            strcpy(user_cfg.nms_id, result);
-        case 3:
-            strcpy(user_cfg.nms_udp_port, result);
-        default:
-            break;
-        }
-        printf("%s\n", result);
-    }
- */
+
 struct cfg get_cfg(int argc, char *argv[])
 {
     struct cfg user_cfg;
     char line[MAX_LEN];
     char *parsed_line;
-    char *file_name = get_params(argc, argv);
+    char *file_name = get_file_name(argc, argv);
     FILE *file = fopen(file_name, "r");
     if (!file)
     {
@@ -150,7 +133,7 @@ char *get_line(char line[], FILE *file)
     exit(1);
 }
 
-char *get_params(int argc, char *argv[])
+char *get_file_name(int argc, char *argv[])
 {
     for (int i = 0; i < argc; i++)
     {
