8df2563c2ab8ed338e202a5d3b06e70156b0532b
k4rr3 <davidcarreras2003@gmail.com>
2023-03-08 00:04:28 +0100

    server side implemention started

--- Server/server.py
+++ Server/server.py
@@ -1,14 +1,89 @@
+#!/usr/bin/env python3.8
 import socket
-
-global server_cfg
-known_devices = []
-buffer_size = 1024
+from enum import Enum
 
 
-def main():
-    read_server_cfg()
-    read_known_devices()
-    attend_reg_requests()
+class Status(Enum):
+    DISCONNECTED = 0xA0
+    WAIT_REG_RESPONSE = 0xA2
+    WAIT_DB_CHECK = 0xA4
+    REGISTERED = 0xA6
+    SEND_ALIVE = 0xA8
+
+
+class PduRegister(Enum):
+    REGISTER_REQ = 0x00
+    REGISTER_ACK = 0x02
+    REGISTER_NACK = 0x04
+    REGISTER_REJ = 0x06
+    ERROR = 0x0F
+
+
+class PduAlive(Enum):
+    ALIVE_INF = 0x10
+    ALIVE_ACK = 0x12
+    ALIVE_NACK = 0x14
+    ALIVE_REJ = 0x16
+
+
+class PduSendCfg(Enum):
+    SEND_FILE = 0x20
+    SEND_DATA = 0x22
+    SEND_ACK = 0x24
+    SEND_NACK = 0x26
+    SEND_REJ = 0x28
+    SEND_END = 0x2A
+
+
+class PduGetCfg(Enum):
+    GET_FILE = 0x30
+    GET_DATA = 0x32
+    GET_ACK = 0x34
+    GET_NACK = 0x36
+    GET_REJ = 0x38
+    GET_END = 0x3A
+
+
+status_names = {
+    Status.DISCONNECTED: "DISCONNECTED",
+    Status.WAIT_REG_RESPONSE: "WAIT_REG_RESPONSE",
+    Status.WAIT_DB_CHECK: "WAIT_DB_CHECK",
+    Status.REGISTERED: "REGISTERED",
+    Status.SEND_ALIVE: "SEND_ALIVE"
+}
+
+pdu_types = {
+    PduRegister.REGISTER_REQ: "REGISTER_REQ",
+    PduRegister.REGISTER_ACK: "REGISTER_ACK",
+    PduRegister.REGISTER_NACK: "REGISTER_NACK",
+    PduRegister.REGISTER_REJ: "REGISTER_REJ",
+    PduRegister.ERROR: "ERROR",
+    PduAlive.ALIVE_INF: "ALIVE_INF",
+    PduAlive.ALIVE_ACK: "ALIVE_ACK",
+    PduAlive.ALIVE_NACK: "ALIVE_NACK",
+    PduAlive.ALIVE_REJ: "ALIVE_REJ",
+    PduSendCfg.SEND_FILE: "SEND_FILE",
+    PduSendCfg.SEND_DATA: "SEND_DATA",
+    PduSendCfg.SEND_ACK: "SEND_ACK",
+    PduSendCfg.SEND_NACK: "SEND_NACK",
+    PduSendCfg.SEND_REJ: "SEND_REJ",
+    PduSendCfg.SEND_END: "SEND_END",
+    PduGetCfg.GET_FILE: "GET_FILE",
+    PduGetCfg.GET_DATA: "GET_DATA",
+    PduGetCfg.GET_ACK: "GET_ACK",
+    PduGetCfg.GET_NACK: "GET_NACK",
+    PduGetCfg.GET_REJ: "GET_REJ",
+    PduGetCfg.GET_END: "GET_END"
+}
+
+
+class Pdu:
+    def __init__(self, pdu_type, system_id, mac_address, random_number, data):
+        self.pdu_type = pdu_type
+        self.system_id = system_id
+        self.mac_address = mac_address
+        self.random_number = random_number
+        self.data = data
 
 
 class Cfg:
@@ -26,6 +101,17 @@ class KnownDevice:
         self.status = status
         self.alea = alea
         self.ip_address = ip_address
+
+
+known_devices = []
+UDP_PKG_SIZE = 78
+TCP_PKG_SIZE = 178
+
+
+def main():
+    read_server_cfg()
+    read_known_devices()
+    attend_reg_requests()
 
 
 def read_server_cfg():
@@ -59,11 +145,21 @@ def read_known_devices():
 
 
 def attend_reg_requests():
-    global buffer_size
+    global UDP_PKG_SIZE
     sockfd_udp = create_udp_socket()
     while True:
-        received_buffer = sockfd_udp.recv(buffer_size)
-        print(received_buffer.decode())
+        received_buffer = sockfd_udp.recv(UDP_PKG_SIZE).decode().split('\x00')
+        received_pdu = [item for item in received_buffer if item != '']
+        if not is_autorized_client(received_pdu[0], received_pdu[1]):
+            print("Cliente NO autorizado")
+            #send REGISTER_REJ amb tots els camps de la pdu a valors zeros i el camp data el motiu de rebuig
+
+def is_autorized_client(id, mac):
+    global known_devices
+    for i in range(len(known_devices)):
+        if known_devices[i].id == id and known_devices[i].mac == mac:
+            return True
+    return False
 
 
 def create_udp_socket():
